6256413793337ca7070b3182ceeed4f4b2478f79
alkomaJunior <alkomajunior@outlook.com>
2021-09-23 21:40:58 +0200

    portfolio show

--- src/controllers/PortfolioController.php
+++ src/controllers/PortfolioController.php
@@ -4,7 +4,9 @@
 namespace app\src\controllers;
 
 
+use app\config\Application;
 use app\config\Controller;
+use app\src\form\PortfolioType;
 use app\src\models\Me;
 use app\src\models\Portfolio;
 use app\src\traits\BackOfficeProtection;
@@ -76,6 +78,62 @@ class PortfolioController extends Controller
             'user'                 => $this::$user,
             '_token'               => $_token,
             'me'                   => (new Me())->findOne(['id' => 1]),
+        ]);
+    }
+
+    public function newPortfolio(){
+
+        (new BackOfficeProtection())->checkForAdminStatus();
+
+        //form building
+        $portfolio = new Portfolio();
+        $portfolioForm = (new PortfolioType($portfolio, "portfolio-new", "post"))->createForm();
+
+        //form validation
+        //check if form is submitted
+        if (Application::$app->request->isPost(Application::$app->request->getMethod())){
+            //getting form data
+            $data = Application::$app->request->getRequestData();
+
+            //hydration of the class
+            $portfolio->loadData($data);
+
+            //check if form is valid and do actions
+            if ($portfolio->isValid()){
+                $portfolio->setImage(rand(1, 9));
+                $portfolio->new();
+                Application::$app->flashMessage->success('Nouveau projet enregistré avec succès.', 'portfolio-index');
+            }
+            else{
+                Application::$app->flashMessage->error("Votre formulaire contient des erreurs....!");
+                Application::$app->flashMessage->display();
+            }
+        }
+
+        echo $this::twig()->render('back-office/portfolio/portfolioNew.html.twig', [
+            'portfolioForm'   => $portfolioForm,
+            'user'        => $this::$user,
+            'request'     => 'add',
+            'me'          => (new Me())->findOne(['id' => 1]),
+        ]);
+    }
+
+    /**
+     * @throws RuntimeError
+     * @throws SyntaxError
+     * @throws LoaderError
+     */
+    public function showPortfolio(){
+
+        (new BackOfficeProtection())->checkForAdminStatus();
+
+        $portfolio = new Portfolio();
+        $myPortfolio = $portfolio->findOne(['id' => (int)$_GET['id']]);
+
+        echo $this::twig()->render('back-office/portfolio/portfolioShow.html.twig', [
+            'myPortfolio'         => $myPortfolio,
+            'user'                => $this::$user,
+            'me'                  => (new Me())->findOne(['id' => 1]),
         ]);
     }
 
